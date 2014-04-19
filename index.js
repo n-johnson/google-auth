820ff68a1c82938fd5b02ebc57ccce43734dfcba
Nathan Johnson <node@njohnson.me>
2014-04-19 06:30:48 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+ * google-auth - index.js
+ * https://github.com/n-johnson/google-auth
+ * Copyright 2014 Nathan Johnson
+ * License: MIT License - https://raw.github.com/n-johnson/google-auth/raw/master/LICENSE
+ */
